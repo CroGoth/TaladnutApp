901a43b6370c5c2519d0ff7c2e0fcf29416de2fc
CroGoth <perintaera@hotmail.com>
2016-12-08 17:31:50 +0700

    Initial commit 1

---
+++ app/src/main/java/crogoth/warcentroid/adminer/taladnutapp/MainActivity.java
@@ -0,0 +1,13 @@
+package crogoth.warcentroid.adminer.taladnutapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
